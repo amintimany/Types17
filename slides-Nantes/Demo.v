b1921f30aecdbaccd55aca59460b40de417052c2
Matthieu Sozeau <mattam@mattam.org>
2017-11-14 12:25:47 +0100

    Final slides.

--- slides-Nantes/Demo.v
+++ slides-Nantes/Demo.v
@@ -20,13 +20,19 @@ Section listlift.
 
 End listlift.
 
-Record Category : Type :=
+Record semigroup@{i} :=
+  { car : Type@{i};
+    op : car -> car -> car;
+    law : forall x y z, op (op x y) z = op x (op y z) }.
+Print semigroup.
+
+Record Category@{i j} : Type :=
 {
   (** Type of Objects *)
-  Obj : Type;
+  Obj : Type@{i};
 
   (** Type of morphism beween two objects *)
-  Hom : Obj -> Obj -> Type;
+  Hom : Obj -> Obj -> Type@{j};
 
   (** composition of morphisms: *)
   compose : forall {a b c : Obj}, (Hom a b) -> (Hom b c) -> (Hom a c);
@@ -36,7 +42,8 @@ Record Category : Type :=
             (compose f (compose g h)) = (compose f (compose g h));
 
   (** symmetric form of associativity: *)
-  assoc_sym : forall {a b c d : Obj} (f : Hom a b) (g : Hom b c) (h : Hom c d),
+  assoc_sym : forall {a b c d : Obj} (f : Hom a b)
+                     (g : Hom b c) (h : Hom c d),
                 (compose f (compose g h )) = (compose f (compose g h));
 
   (** identity morphisms: *)
@@ -82,3 +89,8 @@ Section natlift.
   Definition O_eq : O@{i} = O@{j} := eq_refl.
 
 End natlift.
+
+Parameter properties@{i} : Type@{i} -> Prop.
+
+(* NonCumulative *) Inductive triv_packedType@{i} :=
+  { packed_car : Type@{i}; struct : properties@{i} packed_car }.
